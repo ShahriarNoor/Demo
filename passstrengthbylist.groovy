35da82a0f096044775489be7712a5595d4fe630b
Noor <shahriarnayan15.04@gmail.com>
2022-08-24 13:34:33 +0600

    Got password strength in percentage by method

--- passstrengthbylist.groovy
+++ passstrengthbylist.groovy
@@ -2,7 +2,7 @@ def uppercase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','
 def lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def digit=['0','1','2','3','4','5','6','7','8','9']
 def specialcharacters=['!','@','#','$','%','^','&','*']
-def inputPassword='AAbb123**'
+def inputPassword='123456678'
 def CountOfPasswordLength=10
 def scoreOfUppercase=20
 def scoreOflowercase=20
@@ -10,27 +10,20 @@ def scoreOfDigit=20
 def scoreOfSpecialcharecters=30
 
 def doesPasswordContain(List characterlist, String inputPassword,int score) {
-    def currentcount
-    def characterCountMap = [:]
+
+    def characterMap = [:]
     for (int i = 0; i < characterlist.size(); i++) {
          def currentchar = characterlist[i]
-        characterCountMap.putAt(currentchar, 0)
+        characterMap.putAt(currentchar, 0)
     }
-    //return characterCountMap
     for (int i = 0; i < inputPassword.length(); i++) {
 
-        currentcount = characterCountMap[inputPassword[i]]
-        characterCountMap[inputPassword[i]] = currentcount
-        if (currentcount != null) {
-
+        if (characterMap[inputPassword[i]] != null) {
             return score
-
         }
+
     }
-        if (currentcount==null){
-            return 0
-    }
-   return score
+    return 0
 }
 println(doesPasswordContain(uppercase,inputPassword,scoreOfUppercase))
 println(doesPasswordContain(lowercase,inputPassword,scoreOflowercase))
