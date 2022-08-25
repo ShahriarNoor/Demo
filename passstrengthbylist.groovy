3da1247a6c4451ec0b0a5809e6d7a9d8e7955c47
Noor <shahriarnayan15.04@gmail.com>
2022-08-25 11:34:22 +0600

    got rid of Map

--- passstrengthbylist.groovy
+++ passstrengthbylist.groovy
@@ -2,7 +2,7 @@ def uppercase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','
 def lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def digit=['0','1','2','3','4','5','6','7','8','9']
 def specialcharacters=['!','@','#','$','%','^','&','*']
-def inputPassword='123456678'
+def inputPassword='AAAvvbbssdg123@#$%'
 def CountOfPasswordLength=10
 def scoreOfUppercase=20
 def scoreOflowercase=20
@@ -10,13 +10,10 @@ def scoreOfDigit=20
 def scoreOfSpecialcharecters=30
 
 def doesPasswordContain(List characterlist, String inputPassword,int score) {
-    def characterMap = [:]
-    for (int i = 0; i < characterlist.size(); i++) {
-         def currentchar = characterlist[i]
-        characterMap.putAt(currentchar, 0)
-    }
+
     for (int i = 0; i < inputPassword.length(); i++) {
-        if (characterMap[inputPassword[i]] != null) {
+
+        if (characterlist.contains(inputPassword[i])) {
             return score
         }
 
