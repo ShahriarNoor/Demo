db186828544925ecb286c3fe9c32a8616523758b
Noor <shahriarnayan15.04@gmail.com>
2022-09-21 12:56:03 +0600

    Final Super Reducing String

--- superReducingString.groovy
+++ superReducingString.groovy
@@ -1,21 +1,3 @@
-/*String input="aabbcc"
-def reducingDuplicates(String input){
-    def currentInput=input
-    if (currentInput.length() < 2) {
-        return
-    }
-    def j=0
-    for (int i = 0; i < currentInput.length(); i++){
-        if (currentInput [j] != currentInput [i] ) {
-            j++
-            currentInput
-            currentInput[j] ==currentInput[i]
-
-        }
-    }
-}
-println(reducingDuplicates(input))*/
-
 def superReducedstring(String input){
     for(int i=0;i < input.length();i++){
         def previousString=input[i-1]
