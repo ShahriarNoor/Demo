0918b100e7ddd0b23f921fdbd510136a6b1a0937
Noor <shahriarnayan15.04@gmail.com>
2022-09-26 11:46:39 +0600

    Find how many letters changed

---
+++ marsExploration.groovy
@@ -0,0 +1,19 @@
+def inputString= System.in.newReader().readLine()
+def getChangedWord(String inputString){
+    int counter = 0
+    for(int i= 0; i< inputString.length(); i +=3){
+        if(inputString[i] !='S'){
+            counter ++
+        }
+        if(inputString[i+1] != 'O'){
+            counter ++
+        }
+        if(inputString[i+2] != 'S'){
+            counter ++
+        }
+    }
+    return counter
+
+
+}
+ println(getChangedWord(inputString))
